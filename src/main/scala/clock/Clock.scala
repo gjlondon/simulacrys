4a331d77576ddb895e5add6cf36b91d11ea6f9d0
rogueleaderr <george.j.london@gmail.com>
2018-11-04 21:24:02 -0800

    Try to make the clock ticks tail-recursive
    Otherwise they slow down a loottt after ~100 ticks

--- src/main/scala/clock/Clock.scala
+++ src/main/scala/clock/Clock.scala
@@ -2,21 +2,22 @@ package clock
 
 import world.World
 
+import scala.annotation.tailrec
+
 object Clock {
 
-  def tick(tickNum: Int = 0, maxTicks: Int, world: World): Unit = {
-    if (tickNum >= maxTicks) return
+  @tailrec
+  def tick(tickNum: Int = 0, maxTicks: Int, world: World): World = {
+    if (tickNum >= maxTicks) return world
 
     println(s"Tick number $tickNum")
 
     val newLocations = world.grid.positions map { loc =>
-      val updatedPeople = loc.populace map { p => p.act() }
-      loc.withNewPopulace(populace = updatedPeople)
+      val updatedPopulace = loc.populace map { p => p.act() }
+      loc.withNewPopulace(populace = updatedPopulace)
     }
 
     val newWorld = World.fromLocations(newLocations)
-
-    newWorld.printOverview()
 
     tick(tickNum + 1, maxTicks, newWorld)
   }
